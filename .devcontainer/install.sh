1689100119f79108460fbf763046f5cea0d9bd12
Rom Rakharb <121065878+RomRakharb@users.noreply.github.com>
2025-03-12 20:16:09 +0700

    Update install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -5,14 +5,26 @@ echo "---------------------"
 echo "starts install script"
 echo "---------------------"
 echo
+
 sudo apt update && sudo apt -y upgrade
+
 sudo apt-get install -y software-properties-common
 sudo apt update
+
 sudo add-apt-repository -y ppa:maveonair/helix-editor
 sudo apt update
+
 sudo apt install helix
 cargo install bacon
+cargo install trunk
+cargo install cargo-leptos
+
 rustup component add rust-analyzer
+npm i -g vscode-langservers-extracted
+
+cargo init
+rustup target add wasm32-unknown-unknown
+
 echo
 echo "---------------------"
 echo "install script ends"
