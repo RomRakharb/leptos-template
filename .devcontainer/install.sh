e33543a0675fc249a68199e538681131d270ef0e
Rom Rakharb <121065878+RomRakharb@users.noreply.github.com>
2025-04-08 00:27:20 +0700

    Update install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -11,6 +11,12 @@ npm install npm@latest -g
 
 git clone https://github.com/helix-editor/helix.git
 cargo install --path ./helix/helix-term --locked
+hx --grammar fetch
+hx --grammar build
+cp -r ./helix/runtime/queries ~/.config/helix/runtime/
+cp -r ./helix/runtime/themes ~/.config/helix/runtime/
+cp ./helix/runtime/tutor ~/.config/helix/runtime/
+echo -e "[editor]\ntrue-color = true" > ~/.config/helix/config.toml
 rm -f helix -R
 
 cargo install bacon --locked
